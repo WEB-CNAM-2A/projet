eb2a6a0f0da2dd6080a8d64b35e07a10cbc03037
AIsamet <isamet2067@gmail.com>
2024-06-17 21:39:36 +0200

    created table and connection to db for products

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+// db.js
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Set to true if you have valid SSL certificate, false for self-signed
+  },
+});
+
+module.exports = pool;
--- backend/data/repositories/productRepository.js
+++ backend/data/repositories/productRepository.js
@@ -1,16 +1,16 @@
 // data/repositories/productRepository.js
 
-const fs = require('fs');
-const path = require('path');
-
-// Path to products JSON file
-const productsFilePath = path.join(__dirname, "..", 'products.json');
+const pool = require('../../config/db');
 
 // Repository method to fetch all products
 async function getAll() {
-  // Read product data from JSON file
-  const productsData = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-  return productsData;
+  const client = await pool.connect();
+  try {
+    const res = await client.query('SELECT * FROM products');
+    return res.rows;
+  } finally {
+    client.release();
+  }
 }
 
 module.exports = {
